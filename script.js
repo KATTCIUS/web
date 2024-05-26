f3f48d6b50838ce6a1465206bd74c5d160e9c499
KATTCIUS <kattcius@gmail.com>
2024-05-25 23:00:04 -0300

    actualizar

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
+  var icon = document.querySelector(".icon i");
   if (x.style.display === "block") {
     x.style.display = "none";
+    icon.classList.remove("fa-minus");
+    icon.classList.add("fa-bars");
   } else {
     x.style.display = "block";
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-minus");
   }
 }
